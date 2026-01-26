ce00cae30bb294d7f79c3f5936d6ee58b67256a3
Ivan Porto Carrero <ivan@flanders.co.nz>
2026-01-26 13:53:06 -0800

    disable lto

--- build-grammars.js
+++ build-grammars.js
@@ -336,17 +336,8 @@ async function compileGrammar(grammar, cacheDir, platformDir, platformConfig) {
     cmd.push(
       '-target', platformConfig.zig_target,
       '-O3',
-      '-flto'
+      '-c'
     );
-
-    // LTO on macOS requires LLD linker (must come before -c)
-    if (platformConfig.zig_target.includes('macos')) {
-      cmd.push('-fuse-ld=lld');
-    }
-
-    cmd.push('-c');
-
-
 
     // Common flags
     cmd.push(
